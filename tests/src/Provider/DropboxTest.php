116d22b12925c5359194c2c53d6c0f293c5ca831
Steven Maguire <stevenmaguire@gmail.com>
2017-02-27 21:02:35 -0600

    Update scope test

--- tests/src/Provider/DropboxTest.php
+++ tests/src/Provider/DropboxTest.php
@@ -2,10 +2,13 @@
 
 namespace Stevenmaguire\OAuth2\Client\Test\Provider;
 
+use League\OAuth2\Client\Tool\QueryBuilderTrait;
 use Mockery as m;
 
 class DropboxTest extends \PHPUnit_Framework_TestCase
 {
+    use QueryBuilderTrait;
+
     protected $provider;
 
     protected function setUp()
@@ -42,11 +45,12 @@ class DropboxTest extends \PHPUnit_Framework_TestCase
 
     public function testScopes()
     {
-        $options = ['scope' => [uniqid(),uniqid()]];
-
+        $scopeSeparator = ',';
+        $options = ['scope' => [uniqid(), uniqid()]];
+        $query = ['scope' => implode($scopeSeparator, $options['scope'])];
         $url = $this->provider->getAuthorizationUrl($options);
-
-        $this->assertContains(urlencode(implode(',', $options['scope'])), $url);
+        $encodedScope = $this->buildQueryString($query);
+        $this->assertContains($encodedScope, $url);
     }
 
     public function testGetAuthorizationUrl()
